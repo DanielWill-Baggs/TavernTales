16a8b913ffbe9e0ce9053bf88a4ddc959daf4733
Nick.Trinh <nick.trinh@rimuhc.ca>
2025-01-20 14:43:59 -0500

    feat: added a sample landing page with no functionality

--- src/frontend/tailwind.config.ts
+++ src/frontend/tailwind.config.ts
@@ -3,12 +3,15 @@ import type { Config } from "tailwindcss";
 export default {
     darkMode: ["class"],
     content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
   	extend: {
+		// backgroundImage: {
+		// 	'landing-img': "url('/images/TavernTales-landing-bkg.png')",
+		// },
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
