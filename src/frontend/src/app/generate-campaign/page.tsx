8597e18c60fc039b4ccae2a88a7176d2debdbaef
Daniel Baggs <danielwillbaggs@gmail.com>
2025-01-24 02:42:38 -0500

    feat: finalized one shot campaign front end, design and db save

--- src/frontend/src/app/generate-campaign/page.tsx
+++ src/frontend/src/app/generate-campaign/page.tsx
@@ -1,28 +1,18 @@
 "use client";
 import Image from "next/image";
-import CampaignCreation from "../../public/images/TavernTales-CampaignCreation-bkg.png";
-import { CampaignForm } from "@/components/ui/Campaign/CampaignForm";
 import { CampaignCard } from "@/components/ui/Campaign/CampaignCard";
-import { Button } from "@/components/ui/button";
-
 export default function GenerateCampaign() {
   return (
-    <main className="flex flex-col justify-center items-center text-center h-screen border-black">
-      {/* <div className="relative w-full h-full"> */}
-      <div className="relative w-full h-full z-10">
-        <CampaignForm />
-      </div>
-      <div className="relative w-full h-full z-10">
+    <main className="relative flex flex-col justify-center items-center text-center h-screen border-black">
+      <div className="relative z-10">
         <CampaignCard />
       </div>
-      <Button className="bg-primary text-primary-foreground">Click Me</Button>;
-      {/* <Image
-        src={CampaignCreation}
+      <Image
+        src="/images/TavernTales-CampaignCreation-bkg.png"
         alt="Tavern Tales"
         fill
         className="object-cover z-0"
-      /> */}
-      {/* </div> */}
+      />
     </main>
   );
 }
--- src/frontend/src/components/ui/Campaign/CampaignCard.tsx
+++ src/frontend/src/components/ui/Campaign/CampaignCard.tsx
@@ -1,15 +1,13 @@
 import * as React from "react";
-
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { useEffect, useState } from "react";
+import { CampaignForm } from "./CampaignForm";
 
 export function CampaignCard() {
   const [isClient, setIsClient] = useState(false);
@@ -21,16 +19,28 @@ export function CampaignCard() {
   if (!isClient) return null;
 
   return (
-    <Card className="w-[350px]">
+    // <Card className="w-[850px] h-[650px] bg-brown-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-100 border border-gray-100">
+    <Card
+      className="bg-[url('https://images.pexels.com/photos/235985/pexels-photo-235985.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] drop-shadow-2xl
+ bg-cover bg-center text-brown-900 p-8 rounded-lg shadow-xl border-2 border-yellow-700 w-[850px] mx-auto "
+    >
       <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
+        <CardTitle
+          className=" text-2xl
+"
+        >
+          One-Shot Campaign
+        </CardTitle>
+        <CardDescription
+          className=" text-white-300 text-l
+"
+        >
+          Create your tailored one-shot adventure
+        </CardDescription>
       </CardHeader>
-      <CardContent></CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline">Cancel</Button>
-        <Button>Deploy</Button>
-      </CardFooter>
+      <CardContent>
+        <CampaignForm />
+      </CardContent>
     </Card>
   );
 }
