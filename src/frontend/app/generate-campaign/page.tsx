ba91980879993c22fc93ea55ebb1852b9a8e6861
Daniel Baggs <danielwillbaggs@gmail.com>
2025-01-23 21:30:18 -0500

    chore: making background image the full content of the html dom

--- src/frontend/app/generate-campaign/page.tsx
+++ src/frontend/app/generate-campaign/page.tsx
@@ -1,18 +1,17 @@
 "use client";
 import Image from "next/image";
+import CampaignCreation from "../../public/images/TavernTales-CampaignCreation-bkg.png";
 
 export default function GenerateCampaign() {
   return (
     <div className="relative w-full h-full">
-      <main className="flex flex-col justify-center items-center text-center h-screen">
-        <div className="relative w-full h-screen">
-          <Image
-            src="/images/TavernTales-landing-bkg.png"
-            alt="Tavern Tales"
-            style={{ width: "100%", height: "auto" }}
-          />
-        </div>
-      </main>
+      <Image
+        src={CampaignCreation}
+        alt="Tavern Tales"
+        fill
+        className="object-cover"
+      />
+      <main className="flex flex-col justify-center items-center text-center h-screen"></main>
     </div>
   );
 }
